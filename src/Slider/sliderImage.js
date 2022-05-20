2d60e207c8ef05206e1fb74504723db04c14a3aa
CCylk <connorcylkowski@hotmail.co.uk>
2022-05-20 11:29:53 +0100

    initial commit

---
+++ src/Slider/sliderImage.js
@@ -0,0 +1,31 @@
+import First from "../img/First.png";
+import Second from "../img/Second.png";
+import Third from "../img/Third.png";
+
+export default [
+  {
+    title: "Dave",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+    laborum cupiditate possimus labore, hic temporibus velit dicta earum
+    suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+    voluptatem.`,
+    urls: First,
+  },
+  {
+    title: "Kevin",
+    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+    reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+    quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+    pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+    quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+    Repudiandae, mollitia id reprehenderit a ab odit!`,
+    urls: Second,
+  },
+  {
+    title: "Bob",
+    description: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
+    quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
+    dolor ut sequi minus iste? Quas?`,
+    urls: Third,
+  }
+];
